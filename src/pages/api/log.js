3fb9a90c0cf8fafc4648bf85f8147ba81c9ac45f
Roy <royrmagdaong@gmail.com>
2023-07-31 16:55:42 +0800

    api call to ngrok server and return the data

--- src/pages/api/log.js
+++ src/pages/api/log.js
@@ -1,3 +1,15 @@
+import axios from 'axios'
+
 export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' })
+    // console.log(req)
+    axios.get('https://i-pond-backend.ap.ngrok.io/api/logs')
+    .then(function (response) {
+        // handle success
+        // console.log(response);
+        console.log(response.data)
+        return res.status(200).json({ data: response.data });
+    }).catch(err=>{
+        res.status(500).json({ message: 'error' })
+    })
+    
 }
