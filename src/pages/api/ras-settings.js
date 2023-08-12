33d330b658fd95b76d9ae809dec9c011451e1246
Roy <royrmagdaong@gmail.com>
2023-08-12 22:44:33 +0800

    ras settings api endpoints

---
+++ src/pages/api/ras-settings.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+export default async function handler(req, res) {
+    const token = req.headers['authorization']
+    console.log(token)
+    if(token){
+      return new Promise((resolve, reject) => {
+          axios.get('https://ras-backend.ap.ngrok.io/api/settings', {
+              headers: {
+                  "Authorization": token
+              }
+          })
+            .then(response => {
+            res.status(200).json({ data: response.data });
+            resolve();
+          })
+          .catch(err => {
+            res.status(err.response?.data.error.status).json(err.response?.data)
+            resolve(); // in case something goes wrong in the catch block (as vijay commented)
+          });
+      });
+    }else{
+      return res.status(403).json({message: 'Unauthorized access'})
+    }
+  };
