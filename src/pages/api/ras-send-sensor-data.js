9590c5b9afab66b17a4f563d5c128932fdbcf880
Roy <royrmagdaong@gmail.com>
2023-08-12 17:18:32 +0800

    fixed sending data

--- src/pages/api/ras-send-sensor-data.js
+++ src/pages/api/ras-send-sensor-data.js
@@ -3,7 +3,10 @@ import axios from 'axios'
 export default async function handler(req, res) {
     const token = req.headers['authorization']
     const data = req.body
-    console.log('data', data)
+    // console.log('data', data)
+    console.log('req', req)
+    console.log('body', data)
+    console.log('token', token)
     // console.log(req)
 
     const config = {
@@ -12,15 +15,15 @@ export default async function handler(req, res) {
         }
     }
 
-    return new Promise((resolve, reject) => {
-        axios.post('https://ras-backend.ap.ngrok.io/api/sensor-readings', {data}, config)
+    return await new Promise((resolve, reject) => {
+        axios.post('https://ras-backend.ap.ngrok.io/api/sensor-readings', data, config)
           .then(response => {
-            console.log('data',response.data)
+            // console.log('data', response.data)
             res.status(200).json({ data: response.data });
           resolve();
         })
         .catch(err => {
-            console.log(err)
+            // console.log(err)
             res.status(500).json(err.response?.data)
           resolve(); // in case something goes wrong in the catch block (as vijay commented)
         });
