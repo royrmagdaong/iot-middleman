a8ea917b3408761a9b9d7271e6bf4e9fc67a8325
Roy <royrmagdaong@gmail.com>
2023-08-12 15:32:39 +0800

    ras send sensor data

---
+++ src/pages/api/ras-send-sensor-data.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+export default function handler(req, res) {
+    const token = req.headers['authorization']
+    const data = req.body
+
+    if(token){
+        const config = {
+            headers: {
+                "Authorization": token
+            }
+        }
+        axios.post('https://ras-backend.ap.ngrok.io/api/sensor-readings', data, config)
+        .then( response => {
+            // console.log(response.data)
+            return res.status(200).json({ data: response.data });
+        }).catch(err => {
+            // console.log('Error Response',err.response)
+            return res.status(err.response?.data.error.status).json(err.response?.data)
+        })
+    }else{
+        return res.status(403).json({message: 'Unauthorized access'})
+    }
+}
