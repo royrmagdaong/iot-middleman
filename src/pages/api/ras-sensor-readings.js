14a38371410b21ba7ec1b8a004ed24780c57b686
Roy <royrmagdaong@gmail.com>
2023-08-12 14:55:08 +0800

    edit ras sensor readings api endpoint

--- src/pages/api/ras-sensor-readings.js
+++ src/pages/api/ras-sensor-readings.js
@@ -1,22 +1,46 @@
 import axios from 'axios'
 
-export default function handler(req, res) {
+// export default function handler(req, res) {
+//     const token = req.headers['authorization']
+//     console.log(token)
+//     if(token){
+//         axios.get('https://ras-backend.ap.ngrok.io/api/sensor-readings', {
+//             headers: {
+//                 "Authorization": token
+//             }
+//         })
+//         .then( response => {
+//             // console.log(response.data)
+//             return res.status(200).json({ data: response.data });
+//         }).catch(err => {
+//             // console.log('Error Response',err.response)
+//             return res.status(err.response?.data.error.status).json(err.response?.data)
+//         })
+//     }else{
+//         return res.status(403).json({message: 'Unauthorized access'})
+//     }
+// }
+
+export default async function(req, res) {
     const token = req.headers['authorization']
-    // console.log(token)
-    if(token){
+    console.log(token)
+    return new Promise((resolve, reject) => {
         axios.get('https://ras-backend.ap.ngrok.io/api/sensor-readings', {
             headers: {
                 "Authorization": token
             }
         })
-        .then( response => {
-            // console.log(response.data)
-            return res.status(200).json({ data: response.data });
-        }).catch(err => {
-            // console.log('Error Response',err.response)
-            return res.status(err.response?.data.error.status).json(err.response?.data)
+          .then(response => {
+          res.statusCode = 200
+          res.setHeader('Content-Type', 'application/json');
+          res.setHeader('Cache-Control', 'max-age=180000');
+          res.json({ data: response.data });
+          resolve();
         })
-    }else{
-        return res.status(403).json({message: 'Unauthorized access'})
-    }
-}
+        .catch(err => {
+          res.json(err);
+          res.status(err.response?.data.error.status).json(err.response?.data)
+          resolve(); // in case something goes wrong in the catch block (as vijay commented)
+        });
+    });
+  };
