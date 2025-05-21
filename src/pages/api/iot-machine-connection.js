4e9ba458a371db8eca5739134dfe5ad3479a3791
Roy R. Magdaong <rrmagdaong@BAKALdomain.loc>
2025-05-21 15:23:03 +0800

    comment iot machine app

--- src/pages/api/iot-machine-connection.js
+++ src/pages/api/iot-machine-connection.js
@@ -1,39 +1,39 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-export default async function handler(req, res) {
-    const token = req.headers['authorization']
-    const data = req.body
-    // console.log('data', data.data.machine)
+// export default async function handler(req, res) {
+//     const token = req.headers['authorization']
+//     const data = req.body
+//     // console.log('data', data.data.machine)
 
-    const config = {
-        headers: {
-            "Authorization": token
-        }
-    }
-    if(token){
-      return await new Promise(async (resolve, reject) => {
-        // await axios.get(`https://iot-backend.ap.ngrok.io/api/machine-connections?populate=*&filters[machine][id][$eq]=${data?.data.machine}`, config)
-        await axios.get(`http://10.10.122.91:1340/api/machine-connections?populate=*&filters[machine][id][$eq]=${data?.data.machine}`, config)
-        .then(async response => {
-          let machine_connection = response.data.data[0].id
-          let data2 = {
-            is_connected: true
-          }
-          // await axios.put(`https://iot-backend.ap.ngrok.io/api/machine-connections/${machine_connection}`, {data:data2}, config).then(ress=>{
-          await axios.put(`http://10.10.122.91:1340/api/machine-connections/${machine_connection}`, {data:data2}, config).then(ress=>{
-            console.log('Updated', ress.data)
-            res.status(200).json({ data: ress.data });
-            resolve();
-          }).catch(err=>{res.status(500).json({message: err.message})})
+//     const config = {
+//         headers: {
+//             "Authorization": token
+//         }
+//     }
+//     if(token){
+//       return await new Promise(async (resolve, reject) => {
+//         // await axios.get(`https://iot-backend.ap.ngrok.io/api/machine-connections?populate=*&filters[machine][id][$eq]=${data?.data.machine}`, config)
+//         await axios.get(`http://10.10.122.91:1340/api/machine-connections?populate=*&filters[machine][id][$eq]=${data?.data.machine}`, config)
+//         .then(async response => {
+//           let machine_connection = response.data.data[0].id
+//           let data2 = {
+//             is_connected: true
+//           }
+//           // await axios.put(`https://iot-backend.ap.ngrok.io/api/machine-connections/${machine_connection}`, {data:data2}, config).then(ress=>{
+//           await axios.put(`http://10.10.122.91:1340/api/machine-connections/${machine_connection}`, {data:data2}, config).then(ress=>{
+//             console.log('Updated', ress.data)
+//             res.status(200).json({ data: ress.data });
+//             resolve();
+//           }).catch(err=>{res.status(500).json({message: err.message})})
 
-        }).catch(err=>{res.json({message: "fetch machine connection failed"})})
-        .catch(err => {
-            // console.log(err)
-          res.status(500).json({message:err.message})  
-          resolve(); // in case something goes wrong in the catch block (as vijay commented)
-        });
-      });
-    }else{
-      return res.status(403).json({message: 'Unauthorized access'})
-    }
-}
+//         }).catch(err=>{res.json({message: "fetch machine connection failed"})})
+//         .catch(err => {
+//             // console.log(err)
+//           res.status(500).json({message:err.message})  
+//           resolve(); // in case something goes wrong in the catch block (as vijay commented)
+//         });
+//       });
+//     }else{
+//       return res.status(403).json({message: 'Unauthorized access'})
+//     }
+// }
--- src/pages/api/iot-machine-sensor.js
+++ src/pages/api/iot-machine-sensor.js
@@ -1,34 +1,29 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-export default async function handler(req, res) {
-    const token = req.headers['authorization']
-    const data = req.body
-    // console.log('data', data)
-    // console.log('req', req)
-    // console.log('body', data)
-    // console.log('token', token)
-    // console.log(req)
+// export default async function handler(req, res) {
+//     const token = req.headers['authorization']
+//     const data = req.body
 
-    const config = {
-        headers: {
-            "Authorization": token
-        }
-    }
-    if(token){
-      return await new Promise((resolve, reject) => {
-        axios.post('https://iot-backend.ap.ngrok.io/api/machine-logs', data, config)
-        .then(response => {
-            // console.log('data', response.data)
-          res.status(200).json({ data: response.data });
-          resolve();
-        })
-        .catch(err => {
-            // console.log(err)
-          res.status(500).json(err.response?.data)  
-          resolve(); // in case something goes wrong in the catch block (as vijay commented)
-        });
-      });
-    }else{
-      return res.status(403).json({message: 'Unauthorized access'})
-    }
-}
+//     const config = {
+//         headers: {
+//             "Authorization": token
+//         }
+//     }
+//     if(token){
+//       return await new Promise((resolve, reject) => {
+//         axios.post('https://iot-backend.ap.ngrok.io/api/machine-logs', data, config)
+//         .then(response => {
+//             // console.log('data', response.data)
+//           res.status(200).json({ data: response.data });
+//           resolve();
+//         })
+//         .catch(err => {
+//             // console.log(err)
+//           res.status(500).json(err.response?.data)  
+//           resolve(); // in case something goes wrong in the catch block (as vijay commented)
+//         });
+//       });
+//     }else{
+//       return res.status(403).json({message: 'Unauthorized access'})
+//     }
+// }
